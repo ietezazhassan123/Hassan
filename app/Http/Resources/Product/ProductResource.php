d87e285ad276a799c0a75a16d625870f230493b9
ietezazhassan123 <iete>
2019-11-03 11:58:49 +0500

    Rating and Stoc part

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -21,10 +21,10 @@ class ProductResource extends JsonResource
            'name' =>$this->name ,
            'description' => $this->detail ,
            'price' => $this->price ,
-           'stock' => $this->stock ,
+           'stock' => $this->stock==0 ? 'Out of stock' : $this->stock,
            'discount' => $this->discount,
            'total_Price' => round((1-($this->discount/100)) * $this->price,2),
-           'rating' => round($this->review->sum('star') / $this->review->count(),2),
+           'rating' => round($this->review->sum('star') / $this->review->count(),2) == 0 ? 'No Rating' : round($this->review->sum('star') / $this->review->count(),2),
            'reviews' => route('reviews.index',$this->id)
         ];
     }
